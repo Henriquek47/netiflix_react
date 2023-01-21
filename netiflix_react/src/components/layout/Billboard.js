4bc5efbae585d6f60610a91c01431f074880defa
SilViking <danielsantos_lol@hotmail.com>
2023-01-21 12:40:39 -0300

    Getting started with the homepage billboard

---
+++ netiflix_react/src/components/layout/Billboard.js
@@ -0,0 +1,27 @@
+import classes from './Billboard.module.css';
+import img from '../../images/billboard-img.jpg';
+import movieLogo from '../../images/movie-logo.png';
+
+function Billboard() {
+    return(
+        <div>
+            <span className={classes.billboardContainer}>
+                <div className={classes.billboardRow} role='region'>
+                    <div>
+                        <img className={classes.img} src={img} alt='sem imagem' />
+                        <img className={classes.logo} src={movieLogo} alt='sem imagem'></img>
+                        <h5 className={classes.billboardText}>Desafiado por um professor de música
+                        exigente, o baterista Andrew <br /> toma uma decisão: será um astro do Jazz, a qualquer
+                        custo.<br /> Mesmo que isso destrua sua personalidade.</h5>
+                        <div className={classes.billboardLinks}>
+                            <button type='button' className={classes.button1}>Assistir</button>
+                            <button type='button' className={classes.button2}>Mais Informações</button>
+                        </div>
+                    </div>
+                </div>
+            </span>
+        </div>
+    )
+}        
+        
+export default Billboard;        
